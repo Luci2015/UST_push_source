1105e15030c5fa022fda8cc3ceab09cb4333920a
Lucian Neagu <luci_n24@yahoo.com>
2021-04-12 17:56:15 +0300

    Update extractADevents.ps1

--- extractADevents.ps1
+++ extractADevents.ps1
@@ -3,34 +3,50 @@
 # 4725 - account disabled
 # 4722 - account enabled
 
-# remove old events file
+# remove old input files
 Remove-Item "events.csv"
 Remove-Item "push_list.csv"
-
 # get events since 30 min ago (-0.5):
 $Begin = (Get-Date).AddHours(-0.5)
 $Events = Get-EventLog -logname Security  -After $Begin | where {$_.eventID -eq 4728 -or $_.EventID -eq 4729 -or $_.EventID -eq 4722 -or $_.EventID -eq 4725}
+$already_queried = @{}
+$content = @{}
 foreach($event in $Events){
   $dn = ($event | Select-Object @{Name="dn";Expression={$_.ReplacementStrings[0]}}).dn
+  if ($already_queried.ContainsKey($dn)){
+	  # this user was already queried, so pick the values from hash table
+		$username = $already_queried.$dn.u
+		$remaningGroups = $already_queried.$dn.g
+  } else {  
+		# change extracted Properties as needed
+		$username =  Get-ADUser -Identity $dn -Properties ("mail",
+								   "Country",
+								   "sn",
+								   "GivenName",
+								   "UserPrincipalName",
+								   "MemberOf",
+								   "Enabled")
 
-  # change properties list as needed (eg for custom attributes) and
-  # update the object properties on line 35 to export them correctly to csv
-  $username =  Get-ADUser -Identity $dn -Properties ("mail",
-                           "Country",
-                           "sn",
-                           "GivenName",
-                           "UserPrincipalName",
-                           "MemberOf",
-                           "Enabled")
+		$trgt = @()
+		foreach ($g in $username.MemberOf){ $trgt += (Get-ADGroup $g -Properties "CN").CN }
+		$remaningGroups = $trgt -join ","
+		$content.u = $username
+		$content.g = $remaningGroups
+		$already_queried.$dn = $content
+		}
   
   $eventID = $event.EventID
   if ($eventID -eq "4729"){
   $removedGroup = ($event | Select-Object @{Name="groupName";Expression={ $_.ReplacementStrings[2]}}).groupName
   } else { $removedGroup = "" }
-  $trgt = @()
-  foreach ($g in $username.MemberOf){ $trgt += (Get-ADGroup $g -Properties "CN").CN }
-  $remaningGroups = $trgt -join ","
-  # for username based SSO initialise username & domain properties; initialise the rest where necessary
+  
+  # if needed, change the values of the properties below to 
+  # point to the right variable/value 
+  # because Get-ADUser and Get-ADGroup happens only once per user expect
+  # the output events file to contain the remainingGroup and enabled 
+  # columns populated with whatever values were found at the moment of
+  # the query for MemberOf and Enabled, and not the values at the time of event
+
   New-Object -TypeName PSCustomObject -Property @{
     eventID=$eventID
     timeWritten=$event.TimeWritten
@@ -48,4 +64,6 @@ foreach($event in $Events){
     customAttribute4=""
     remaningGroups="'" + $remaningGroups + "'" } | Export-Csv "events.csv" -NoTypeInformation -Append
 }
+
 (Get-Content "events.csv").replace('"', '').replace("'",'"')| Set-Content "events.csv"
+
