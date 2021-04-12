6bf86e5b3b08284ae92ecac16faa19fd01e9585b
Lucian <lneagu@adobe.com>
2021-04-13 00:06:12 +0300

    Update extractADevents.ps1

--- extractADevents.ps1
+++ extractADevents.ps1
@@ -10,33 +10,32 @@ Remove-Item "events.csv"
 Remove-Item "push_list.csv"
 # get events since 30 min ago (-0.5):
 $Begin = (Get-Date).AddHours(-0.5)
-$Events = Get-EventLog -logname Security  -After $Begin | where {$_.eventID -eq 4728 -or $_.EventID -eq 4729 -or $_.EventID -eq 4722 -or $_.EventID -eq 4725 -or $_.EventID -eq 4756 -or $_.EventID -eq 4757}
+$Events = Get-EventLog -logname Security  -After $Begin | where { $_.eventID -eq 4728 -or $_.EventID -eq 4729 -or $_.EventID -eq 4722 -or $_.EventID -eq 4725 -or $_.EventID -eq 4756 -or $_.EventID -eq 4757}
 $already_queried = @{}
 $content = @{}
 foreach($event in $Events){
   $dn = ($event | Select-Object @{Name="dn";Expression={$_.ReplacementStrings[0]}}).dn
   if ($already_queried.ContainsKey($dn)){
-	  # this user was already queried, so pick the values from hash table
-		$username = $already_queried.$dn.u
-		$remaningGroups = $already_queried.$dn.g
+	# this user was already queried, so pick the values from hash table
+	$username = $already_queried.$dn.u
+	$remaningGroups = $already_queried.$dn.g
   } else {  
-		# change extracted Properties as needed
-		$username =  Get-ADUser -Identity $dn -Properties ("mail",
-								   "Country",
-								   "sn",
-								   "GivenName",
-								   "UserPrincipalName",
-								   "MemberOf",
-								   "Enabled")
+	# change extracted Properties as needed
+        $username =  Get-ADUser -Identity $dn -Properties ("mail",
+                                                           "Country",
+                                                           "sn",
+                                                           "GivenName",
+                                                           "UserPrincipalName",
+                                                           "MemberOf",
+                                                           "Enabled")
+        $trgt = @()
+        foreach ($g in $username.MemberOf){ $trgt += (Get-ADGroup $g -Properties "CN").CN }
+        $remaningGroups = $trgt -join ","
+        $content.u = $username
+        $content.g = $remaningGroups
+        $already_queried.$dn = $content
+   }
 
-		$trgt = @()
-		foreach ($g in $username.MemberOf){ $trgt += (Get-ADGroup $g -Properties "CN").CN }
-		$remaningGroups = $trgt -join ","
-		$content.u = $username
-		$content.g = $remaningGroups
-		$already_queried.$dn = $content
-		}
-  
   $eventID = $event.EventID
   if ($eventID -eq "4729" -or $eventID -eq "4757"){
   $removedGroup = ($event | Select-Object @{Name="groupName";Expression={ $_.ReplacementStrings[2]}}).groupName
